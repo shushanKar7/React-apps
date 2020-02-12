4908133a42d14570887c5f7617d88fdd80222a2c
shushanKar7 <shushan.kar97@gmail.com>
2020-02-12 19:08:03 +0400

    Todo App with Raect

---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+import "./item-add-form.css";
+
+class ItemAddForm extends Component {
+  state = {
+    label: ""
+  };
+
+  onLabelChange = e => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ''
+    })
+  };
+
+  render() {
+    return (
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          onChange={this.onLabelChange}
+          className="form-input"
+          placeholder="What needs to be done"
+          value={this.state.label}
+        />
+        <button className="btn btn-outline-secondary">Add item</button>
+      </form>
+    );
+  }
+}
+
+export default ItemAddForm;
